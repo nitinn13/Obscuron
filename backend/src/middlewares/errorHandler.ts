7b0904214b77737ff918aee76264477da8eaf071
Nitin Jha <nitinjha@Nitins-MacBook-Air.local>
2025-09-03 15:58:26 +0530

    Update backend code

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,20 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
-const errorHandler = (error: Error, req: Request, res: Response) => {
+const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(error.name);
 
-    if(error.name === "CastError") {
-        res.status(400).json({ message: "Malformatted Id" });
-    }
-    else if(error.name === "JsonWebTokenError") {
+    if (error.name === "CastError") {
+        return res.status(400).json({ message: "Malformatted Id" });
+    } 
+    else if (error.name === "JsonWebTokenError") {
         console.log(error);
-        res.status(400).json({ message: "Invalid token" });
-    }
+        return res.status(400).json({ message: "Invalid token" });
+    } 
     else {
-        res.status(500).json({
-            message: "Something went wrong"
-        });
+        return res.status(500).json({ message: "Something went wrong" });
     }
-}
+};
 
 export default errorHandler;
